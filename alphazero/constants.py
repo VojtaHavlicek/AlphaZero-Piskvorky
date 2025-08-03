a5b666cccda2252db575f48a8671e9a77e96fb75
Vojtech Havlicek <vojta.havlicek@gmail.com>
2025-08-03 18:03:00 -0400

    7x7_5 first attempt at training

--- alphazero/constants.py
+++ alphazero/constants.py
@@ -1,8 +1,8 @@
 # --- Parameters ---
-BOARD_SIZE = 5
-WIN_LENGTH = 4
+BOARD_SIZE = 7
+WIN_LENGTH = 5
 
-NUM_EPISODES = 10
+NUM_EPISODES = 50
 NUM_WORKERS = 6 # Adjust based on your CPU cores.
 
 DEVICE = "cpu"
@@ -13,18 +13,18 @@ O = "O"
 DRAW = "D"
 
 # ---- SELF-PLAY PARAMETERS ----
-NUM_SELF_PLAY_GAMES = 100 # 100-500 for TicTacToe, 1_000-10_000 for Gomoku
+NUM_SELF_PLAY_GAMES = 200 # 100-500 for TicTacToe, 1_000-10_000 for Gomoku
 NUM_SELF_PLAY_SIMULATIONS = 200  # Number of MCTS simulations per move.
-SELF_PLAY_EXPLORATION_CONSTANT = 2.0  # Exploration constant for MCTS
-BUFFER_CAPACITY = 40_000
+SELF_PLAY_EXPLORATION_CONSTANT = 3.0  # Exploration constant for MCTS
+BUFFER_CAPACITY = 40_000 
 TEMPERATURE_SCHEDULE_HALFTIME = 100
-TEMPERATURE_BASELINE = 0.01  # Base temperature for self-play
+TEMPERATURE_BASELINE = 0.005  # Base temperature for self-play
 
 # ---- TRAINING PARAMETERS ----
-BATCH_SIZE = 1024
-BATCHES_PER_EPISODE = 10 # How many batches to train on each episode
+BATCH_SIZE = 2024
+BATCHES_PER_EPISODE = 15 # How many batches to train on each episode
 NUM_EPOCHS = 3 # How many epochs to train on each batch of data? 
-LEARNING_RATE = 5*1e-5# Initial learning rate for the optimizer
+LEARNING_RATE = 1e-4# Initial learning rate for the optimizer
 MODEL_DIR = "models"
 
 # ---- EVAL PARAMETERS ----
@@ -32,4 +32,4 @@ EVALUATION_GAMES = 51
 NUM_EVAL_SIMULATIONS = 200  # Number of MCTS simulations for evaluation
 EVAL_EXPLORATION_CONSTANT = 2.0  # Exploration constant for MCTS during evaluation
 EVAL_TEMPERATURE = 0.3 # Temperature for evaluation (lower means more greedy)
-EVAL_TEMPERATURE_SCHEDULE_HALFTIME = 4  # Temperature decay for evaluation
+EVAL_TEMPERATURE_SCHEDULE_HALFTIME = 5 # Temperature decay for evaluation
